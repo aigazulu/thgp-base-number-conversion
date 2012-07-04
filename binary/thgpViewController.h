e8cb0316273ceb2ca9f899575c807b13338a7e4c
aiga <zulu@labs.hiddengeniusproject.org>
2012-07-03 17:25:50 -0700

    code
    picker doesn't work

--- binary/thgpViewController.h
+++ binary/thgpViewController.h
@@ -18,13 +18,5 @@
 @property (nonatomic, retain) IBOutlet UITextField *inputText;
 @property (nonatomic, retain) IBOutlet UITextField *outputText;
 
--(IBAction)base2:(id)sender;
--(IBAction)base3:(id)sender;
--(IBAction)base4:(id)sender;
--(IBAction)base5:(id)sender;
--(IBAction)base6:(id)sender;
--(IBAction)base7:(id)sender;
--(IBAction)base8:(id)sender;
--(IBAction)base9:(id)sender;
-
+-(IBAction)converter:(id)sender;
 @end
